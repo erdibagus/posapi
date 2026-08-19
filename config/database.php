b76b4d07f3154955b691f8640708813737e15f9e
erdibagus <erdibgs@gmail.com>
2026-08-19 15:33:48 +0700

    test db

--- config/database.php
+++ config/database.php
@@ -12,8 +12,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 
 $host = 'localhost';
 $db_name = 'kumonpur_pos';
-$username = 'kumonpur_baguffs';
-$password = 'gagaso123!';
+$username = 'kumonpur_bagus';
+$password = 'Gagaso123!';
 // $db_name = 'posprime_db';
 // $username = 'root';
 // $password = '';
