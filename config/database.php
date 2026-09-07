19fbaa3a90b9924c3109fc13dac4af512a94659e
erdibagus <erdibgs@gmail.com>
2026-09-07 13:59:47 +0700

    update no cache

--- config/database.php
+++ config/database.php
@@ -37,6 +37,11 @@ function cors() {
 }
 
 function res($status, $data = null, $message = '') {
+    // Disable cache untuk semua API response
+    header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+    header('Pragma: no-cache');
+    header('Expires: 0');
+    
     echo json_encode(['status' => $status, 'data' => $data, 'message' => $message]);
     exit;
 }
